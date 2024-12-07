10a7c67563a50cfa3032ba69c9c3bc66ba3e5185
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-07 21:10:55 +0530

    Add files via upload

---
+++ prgmbreak.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class prgmbreak{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=0;i<=n;i++)
+        {
+            if(i==2)
+            break;
+        System.out.println(i);
+        }
+    }
+}
