4e516b57bfba530bdc236ae525392a218d353f64
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-07 21:09:34 +0530

    Add files via upload

---
+++ programc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class programc{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=0;i<=n;i++)
+        {
+            if(i==2)
+            continue;
+        System.out.println(i);
+        }
+    }
+}
